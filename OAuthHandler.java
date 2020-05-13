edfd9baab02e47c5f36cefc9921388aed6b6f77d
ananyamitra <anon84@gmail.com>
2020-05-13 13:13:15 -0400

    Create OAuthHandler.java
    1. OAuthHandler can be placed in the same package from where we call gettoken Currently
    2. The package string in OAuthHandler needs to be changed to have the package name it was copied to
    3. Put the clientId and tokenUrl correctly in the OAuthhandler file
    4. In the code where we call getToken now, we can comment the getToken call and instead do this:
    
    	OAuthHandler oauthHandler = new OAuthHandler();
    	String token = oauthHandler.getToken();

---
+++ OAuthHandler.java
@@ -0,0 +1,69 @@
+package com.test.rest;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSocketFactory;
+import java.io.*;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class OAuthHandler {
+
+    private static final Pattern pat = Pattern.compile(".*\"access_token\"\\s*:\\s*\"([^\"]+)\".*");
+    private static final String clientId = "Put client Id here";//clientId
+    private static final String tokenUrl = "Put Token URL here";
+
+    private static String getTokenfromAuthServer() {
+        String content = "grant_type=client_credentials&client_id=" + clientId;
+        BufferedReader reader = null;
+        HttpsURLConnection connection = null;
+        String returnValue = "";
+        try {
+            URL url = new URL(tokenUrl);
+            connection = (HttpsURLConnection) url.openConnection();
+
+            SSLSocketFactory sslsocketfactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+            connection.setSSLSocketFactory(sslsocketfactory);
+
+            connection.setRequestMethod("POST");
+            connection.setDoOutput(true);
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            connection.setRequestProperty("Accept", "application/json");
+            PrintStream os = new PrintStream(connection.getOutputStream());
+            os.print(content);
+            os.close();
+            reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line = null;
+            StringWriter out = new StringWriter(connection.getContentLength() > 0 ? connection.getContentLength() : 2048);
+            while ((line = reader.readLine()) != null) {
+                out.append(line);
+            }
+            String response = out.toString();
+            System.out.println(response);
+            Matcher matcher = pat.matcher(response);
+            if (matcher.matches() && matcher.groupCount() > 0) {
+                returnValue = matcher.group(1);
+            }
+        } catch (Exception e) {
+            System.out.println("Error : " + e.getMessage());
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                }
+            }
+            connection.disconnect();
+        }
+        return returnValue;
+    }
+
+    public String getToken() {
+
+        return getTokenfromAuthServer();
+
+    }
+}
+
+
